33e41c2ea6a300f9fe1e0d171af2d39067724d7e
dhruvi ghodasara <dhruvighodasara03@gmail.com>
2026-04-09 00:55:05 -0500

    product-list modified

---
+++ src/Components/Products/ProductList.js
@@ -0,0 +1,137 @@
+import babyshirt from "../../assets/baby-shirt.png";
+import Dress from "../../assets/dress.png";
+import Hoodie from "../../assets/hoodie.png";
+import Jeans from "../../assets/jeans.png";
+import LeatherJacket from "../../assets/leather-jacket.png";
+import Shirtdress from "../../assets/shirt-dress.png";
+import Shirt from "../../assets/shirt.png";
+import Skater from "../../assets/skater.png";
+import Skirt from "../../assets/skirt.png";
+import Sleepsuit from "../../assets/sleepsuit.png";
+import Sweater from "../../assets/sweater.png";
+import Tshirt from "../../assets/tshirt.png";
+
+const ProductList = [
+  {
+    id: 1,
+    image: babyshirt,
+    name: "Baby T-Shirt",
+    price: 25,
+    oldPrice: 40,
+    onSale: true,
+    newArrival: false,
+    category: "Kids",
+  },
+  {
+    id: 2,
+    image: Dress,
+    name: "Dress",
+    price: 60,
+    oldPrice: 80,
+    onSale: false,
+    newArrival: true,
+    category: "Kids",
+  },
+  {
+    id: 3,
+    image: Hoodie,
+    name: "Hoodie",
+    price: 70,
+    oldPrice: 0,
+    onSale: false,
+    newArrival: true,
+    category: "Mens",
+  },
+  {
+    id: 4,
+    image: Jeans,
+    name: "Jeans",
+    price: 55,
+    oldPrice: 75,
+    onSale: true,
+    newArrival: false,
+    category: "Mens",
+  },
+  {
+    id: 5,
+    image: LeatherJacket,
+    name: "Leather Jacket",
+    price: 120,
+    oldPrice: 150,
+    onSale: true,
+    newArrival: false,
+    category: "Mens",
+  },
+  {
+    id: 6,
+    image: Shirtdress,
+    name: "Shirt Dress",
+    price: 65,
+    oldPrice: 0,
+    onSale: false,
+    newArrival: true,
+    category: "Womens",
+  },
+  {
+    id: 7,
+    image: Shirt,
+    name: "Shirt",
+    price: 35,
+    oldPrice: 50,
+    onSale: true,
+    newArrival: false,
+    category: "Mens",
+  },
+  {
+    id: 8,
+    image: Skater,
+    name: "Skater",
+    price: 45,
+    oldPrice: 0,
+    onSale: false,
+    newArrival: true,
+    category: "Womens",
+  },
+  {
+    id: 9,
+    image: Skirt,
+    name: "Skirt",
+    price: 40,
+    oldPrice: 55,
+    onSale: true,
+    newArrival: false,
+    category: "Womens",
+  },
+  {
+    id: 10,
+    image: Sleepsuit,
+    name: "Sleepsuit",
+    price: 30,
+    oldPrice: 45,
+    onSale: true,
+    newArrival: false,
+    category: "Kids",
+  },
+  {
+    id: 11,
+    image: Sweater,
+    name: "Sweater",
+    price: 75,
+    oldPrice: 95,
+    onSale: true,
+    newArrival: false,
+    category: "Kids",
+  },
+  {
+    id: 12,
+    image: Tshirt,
+    name: "T-Shirt",
+    price: 28,
+    oldPrice: 0,
+    onSale: false,
+    newArrival: true,
+    category: "Mens",
+  },
+];
+
+export default ProductList;
